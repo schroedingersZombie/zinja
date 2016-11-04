6124e644ac7a014e75ad4ac51054e24d915dff3c
Leon Strauss <leon@ilu.st>
2016-11-04 19:22:29 +0100

    continued working on publishing

--- zinja.js
+++ zinja.js
@@ -9,6 +9,7 @@ var request = require('request');
 var inquirer = require('inquirer');
 var cache = require('persistent-cache');
 var temp = require('temp').track();
+var columns = require('cli-columns');
 
 var name = basename(process.argv[1], '.js');
 
@@ -29,7 +30,7 @@ program
 
 program
     .command('* [command-arguments...]')
-    .description('Execute the soke script * (for example \'' + name + ' hello-world\' will execute the hello-world script)')
+    .description('Execute the zinja script * (for example \'' + name + ' hello-world\' will execute the hello-world script)')
     .action(execute);
 
 program
@@ -43,18 +44,18 @@ program
     .action(unregister);
 
 program
-    .command('publish <name> <file>')
-    .description('Publishes the given script to the central soke repository')
+    .command('publish <file>')
+    .description('Publishes the given script to the central zinja repository')
     .action(publish);
 
 program
     .command('republish <name> <file>')
-    .description('Republishes (updates) the given script to the central soke repository')
+    .description('Republishes (updates) the given script to the central zinja repository')
     .action(toBeImplemented);
 
 program
     .command('search <query>')
-    .description('Searches the central soke repository for scripts matching the query')
+    .description('Searches the central zinja repository for scripts matching the query')
     .action(search);
 
 program
@@ -73,7 +74,7 @@ if(!process.argv.slice(2).length) {
 }
 
 function onConnectionProblem() {
-    console.error('Could not connect to the repository, maybe there is a problem with your internet connection or we are currently under maintenance? Check soke.io for updates or try again later');
+    console.error('Could not connect to the repository, maybe there is a problem with your internet connection or we are currently under maintenance? Check zinja.io for updates or try again later');
     process.exit(1);
 }
 
@@ -131,7 +132,7 @@ function fetchRemoteScript(name, cb) {
 
         if(response.statusCode != 200) {
             if(response.statusCode == 404) {
-                console.error('Script ' + name + ' was not found in the central soke repository. Try ' + name + ' search ' + name);
+                console.error('Script ' + name + ' was not found in the central zinja repository. Try ' + name + ' search ' + name);
                 cb(404);
             }
 
@@ -161,14 +162,15 @@ function info(name) {
 function executeScript(script, args) {
     var tempFilePath = '';
 
-    temp.open('soke', function tempFileCreated(err, tmpFile) {
+    temp.open('zinja', function tempFileCreated(err, tmpFile) {
         tempFilePath = tmpFile.path;
         fs.write(tmpFile.fd, script, onFileWritten);
     });
 
     function onFileWritten(err) {
         if(err != null) {
-            console.error('Could not write script temp file');
+            console.error('Could not write script temp file:');
+            console.error(err);
             process.exit(1);
         }
 
@@ -222,22 +224,17 @@ function unregister(name) {
     })
 }
 
-function publish(name, fileName) {
-    if(!name.match(/^[a-z]+(-[a-z0-9]+)*$/)) {
-        console.error('Invalid name. Script names can only contain lowercase letters, numbers and dashes and must begin with a letter');
-        console.error('Examples: some-script a43-reset foo bar-6');
-
-        process.exit(1);
-    }
-
+function publish(fileName) {
     inquirer.prompt([{
         message: 'Enter the name the script should be published under:',
         name: 'name',
-        type: 'input'
-    }, {
-        message: 'Repeat the name for the script:',
-        name: 'nameRepeat',
-        type: 'input'
+        type: 'input',
+        validate: function(name) {
+            if(!name.match(/^[a-z]+(-[a-z0-9]+)*$/))
+                return 'Invalid name. Script names can only contain lowercase letters, numbers and dashes and must begin with a letter';
+
+            return true;
+        }
     }, {
         message: 'Enter your username (if you haven\'t one yet, run zj register-new-user):',
         name: 'user',
@@ -257,7 +254,7 @@ function publish(name, fileName) {
 
             request({
                 body: {
-                    name: name,
+                    name: answers.name,
                     script: content
                 },
                 uri: scriptsEndpoint,
@@ -269,20 +266,27 @@ function publish(name, fileName) {
                 }
             }, onResponse);
         }
+
+        function onResponse(err, response, body) {
+            if(err != null) {
+                return onConnectionProblem();
+            }
+
+            switch (response.statusCode) {
+                case 201:
+                    console.log('Successfully published script ' + answers.name);
+                    break;
+                case 401:
+                    console.error('Authentication failed');
+                    process.exit(1);
+                case 409:
+                    console.error('A script with that name already exists');
+                    process.exit(1);
+                default:
+                    console.error('There was some problem with the server. Try again later.');
+            }
+        }
     });
-
-    function onResponse(err, response, body) {
-        if(err != null) {
-            return onConnectionProblem();
-        }
-
-        if(response.statusCode != 201) {
-            console.error('Could not publish, maybe that name is already taken? Try ' + name + ' search ' + name);
-            process.exit(1);
-        }
-
-        console.log('Successfully published script ' + name);
-    }
  }
 
 function search(query) {
@@ -299,6 +303,6 @@ function search(query) {
             return onConnectionProblem();
         }
 
-        console.log(JSON.parse(body).join('\n'));
+        console.log(columns(JSON.parse(body)));
     }
 }
