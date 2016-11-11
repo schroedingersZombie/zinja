3ccbe8113d5cb5e263d1bc447bcb2e6997d8c5b1
Leon Strauss <leon@ilu.st>
2016-11-11 21:48:45 +0100

    info now also seacrhes the local scripts and states the source

--- info.js
+++ info.js
@@ -6,12 +6,22 @@ const cache = require('persistent-cache');
 const onConnectionProblem = require('./connection-problem');
 const scriptsEndpoint = require('./config').api.scripts;
 const remoteCache = cache({ duration: 1000 * 3600 * 24 * 7 });
+const localScripts = cache({ name: 'local' });
+
+function isLocalScript(name) {
+    return name.indexOf('/') == -1;
+}
 
 function info(name) {
-    request.get({
-        uri: scriptsEndpoint + '/' + name + '/info',
-        json: true
-    }, function(err, response, scriptInfo) {
+    if(isLocalScript(name))
+        return localScripts.get(name, onLocalCache);
+    else
+        return request.get({
+            uri: scriptsEndpoint + '/' + name + '/info',
+            json: true
+        }, onResponse);
+
+    function onResponse(err, response, scriptInfo) {
         if (err != null) {
             onConnectionProblem();
             process.exit(1);
@@ -29,25 +39,53 @@ function info(name) {
 
         remoteCache.put(name.replace('/', 'u'), scriptInfo.script, assertError);
 
-        var output = {
+        outputScriptInfo({
             'Name:': scriptInfo.name,
-            'Author:': scriptInfo.user
-        };
+            'Author:': scriptInfo.user,
+            'Source:': 'Zinja Central'
+        }, {
+            script: scriptInfo.script,
+            description: scriptInfo.description
+        });
+    }
 
-        console.log(columnify(output, { showHeaders: false }));
-
-        if (scriptInfo.description) {
-            console.log();
-            console.log('Description');
-            console.log('------------');
-            console.log(scriptInfo.description);
+    function onLocalCache(err, script) {
+        if(err != null) {
+            console.error(err);
+            process.exit(1);
         }
 
+        if(script === undefined)
+            return cb('Script ' + name + ' was not found in the local repository');
+
+        outputScriptInfo({
+            'Name:': name,
+            'Source:': 'Local Repository'
+        }, {
+            script: script
+        });
+    }
+}
+
+function outputScriptInfo(metadata, scriptInfo) {
+    var output = {
+        'Name:': scriptInfo.name,
+        'Author:': scriptInfo.user
+    };
+
+    console.log(columnify(metadata, { showHeaders: false }));
+
+    if (scriptInfo.description) {
         console.log();
-        console.log('Source');
+        console.log('Description');
         console.log('------------');
-        console.log(scriptInfo.script);
-    });
+        console.log(scriptInfo.description);
+    }
+
+    console.log();
+    console.log('Source');
+    console.log('------------');
+    console.log(scriptInfo.script);
 }
 
 module.exports = info;
